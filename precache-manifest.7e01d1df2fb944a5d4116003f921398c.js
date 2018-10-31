93b95521df15509571c7d8e9ecd63deaac70b454
bakir97 <you@example.com>
2018-10-31 16:45:00 +0100

    prvi

---
+++ precache-manifest.7e01d1df2fb944a5d4116003f921398c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7e1e7811c69c2142a5206348da8d89c",
+    "url": "/static/media/global_shopping_experience.d7e1e781.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9359e8c227662bdb4f91",
+    "url": "/static/js/main.9359e8c2.chunk.js"
+  },
+  {
+    "revision": "005360e40e83edbbc88a",
+    "url": "/static/js/1.005360e4.chunk.js"
+  },
+  {
+    "revision": "9359e8c227662bdb4f91",
+    "url": "/static/css/main.f80b06a4.chunk.css"
+  },
+  {
+    "revision": "7dfa5b7354bc73a0df700835271ce817",
+    "url": "/index.html"
+  }
+];
